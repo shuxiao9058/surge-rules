17868e8d38e9ebfb983b472e3fd89764f54552f7
Aaron Ji <shuxiao9058@gmail.com>
2024-04-26 21:18:44 +0800

    add 17usoft-zh-cn-locale.js

---
+++ scripts/17usoft-zh-cn-locale.js
@@ -0,0 +1,27 @@
+
+let headers = $request.headers;
+
+if (headers) {
+    let cookie = headers['Cookie'];
+    if (cookie && cookie.includes("Lang=en-us")) {
+        cookie = cookie.replace("Lang=en-us", "Lang=zh-cn");
+        headers['Cookie'] = cookie;
+        $request.headers = headers;
+    }
+
+    if (headers['Lang']) {
+        headers['Lang'] = "zh-cn"
+    }
+
+    if (headers['Accept-Language']) {
+        headers['Accept-Language'] = "zh,zh-CN;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6"
+    }
+    $request.headers = headers;
+}
+
+const result = {};
+result.headers = headers;
+
+$done(result);
+
+
